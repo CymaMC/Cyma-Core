6d07ce4cb8f286d26e95785eec40aea50f7f27a0
Cyma <89592986+CymaMC@users.noreply.github.com>
2021-08-27 02:49:52 +1200

    Update Cyma.java

--- src/main/java/com/emopedia/Cyma.java
+++ src/main/java/com/emopedia/Cyma.java
@@ -1,1 +1,29 @@
+package com.emopedia;
+
+import com.emopedia.commands.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class Cyma extends JavaPlugin {
+
+    private Cyma plugin;
+
+    @Override
+    public void onEnable() {
+
+        System.out.println("The core plugin for Cyma is now active.");
+
+        Objects.requireNonNull(this.getCommand("start")).setExecutor(new StartCommand(this));
+        Objects.requireNonNull(this.getCommand("rules")).setExecutor(new RulesCommand(this));
+        Objects.requireNonNull(this.getCommand("discord")).setExecutor(new DiscordCommand(this));
+        Objects.requireNonNull(this.getCommand("fly")).setExecutor(new FlyCommand(this));
+        Objects.requireNonNull(this.getCommand("cyma")).setExecutor(new ReloadCommand(this));
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+
+    }
+}
 
